a5e7ce77d0d9fbb91b0a64b15ddc987a56d29ec3
cvmahaniagaartha <cvmahaniagaartha.sms@gmail.com>
2026-04-20 03:03:51 +0000

    fix maps

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+
+import "leaflet/dist/leaflet.css"; // 🔥 HARUS di atas
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
